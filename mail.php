d4e04f8aa1ffe020de829d859903642eff34f5f1
fabiomontefuscolo <fabio.montefuscolo@gmail.com>
2009-02-16 22:01:43 +0000

    git-svn-id: http://svn.reddes.bvsalud.org/iahx/trunk/opac@11 80a35789-0352-4f55-950b-1782766af574

--- mail.php
+++ mail.php
@@ -8,73 +8,85 @@
  	require_once("./classes/class.phpmailer.php");
 
 	// Registrar caminho completo para os arquivos de imagem e CSS
-	$dia_path = "http://" . $config->SERVERNAME . $config->PATH_DATA;
-	
+    $dia_path = "http://" . $config['SERVERNAME'] . $config['PATH_DATA'];
+
+    $q  = stripslashes($_POST["q"]);
 	// Formar consulta a partir dos favoritos marcados
-	session_start();
-	if( isset($_SESSION["bookmark"]) ){
-		$bookmark = unserialize($_SESSION["bookmark"]);
-		$q = "\"".$bookmark->list[0]."\"";
-		$len = sizeOf($bookmark->list);
-		for($i = 1; $i < $len; $i=$i+1){
-			$q .= " OR "."\"".$bookmark->list[$i]."\""; 
-		}
-		$q = "id:(". $q .")";
-	}else{
-		die("Error");
-	}
-	
-	// Dados do endereço eletrônico
-	$recipientMail = $_POST['recipientMail'];
-	$senderMail = $_POST['senderMail'];
-	$subject = $_POST['subject'];
-	$lang = $_POST['lang'];
-	
-	$col = $_REQUEST["col"];
-	$site = $_REQUEST["site"];
-	
-	if ( !isset($col) ){
-		$col = $defaultCollection; 				// valor default criada no script de configuracao do sistema
-		$VARS["col"] = $col;					// registra no array vars para uso na XSL
-	}
-	
-	if ( !isset($site) ){	
-		$site= $defaultSite;					// valor default criada no script de configuracao do sistema
-		$VARS["site"] = $site;					// registra no array vars para uso na XSL
-	}
+    if( $_POST["option"] == 'from_to' ){
+        $from = ( isset($_POST["from"]) ? abs( $_POST["from"] ) : 1 );
+        $count = ( isset($_POST["count"]) ? abs( $_POST["count"] ) : sizeOf($bookmark->list) );
+    }else if( $_POST["option"] == 'selected' ){
+        session_start();
+        if( isset($_SESSION["bookmark"]) ){
+            $bookmark = unserialize($_SESSION["bookmark"]);
+            $q = "\"".$bookmark->list[0]."\"";
+            $len = sizeOf($bookmark->list);
+            for($i = 1; $i < $len; $i=$i+1){
+                $q .= " OR "."\"".$bookmark->list[$i]."\"";
+            }
+            $q = "id:(". $q .")";
 
-	$from = ( isset($_REQUEST["from"]) ? $_REQUEST["from"] : 0 );
-	
-	$index= $_REQUEST["index"];
-	$sort = $_REQUEST["sort"]; 		      		//sort parameter
-	$output = ( isset($_REQUEST["output"]) && $_REQUEST["output"] != '' ? $_REQUEST["output"] : "json" );
-		
+            $from = 1;
+            $count = sizeOf($bookmark->list);
+        }else{
+            die("Error");
+        }
+    }
+    
+    // Dados do endereço eletrônico
+    $recipientMail = $_POST['recipientMail'];
+    $senderMail = $_POST['senderMail'];
+    $subject = $_POST['subject'];
+    $lang = $_POST['lang'];
+    
+    if($_POST['option'] != 'query_only' ){
+        $col = $_REQUEST["col"];
+        $site = $_REQUEST["site"];
 
-	$count = sizeOf($bookmark->list);
-	$VARS["count"] = $count;
-	
-	//DIA server connection object
-	$dia = new Dia($site, $col, $count, $output, $lang);	
-	$page= new Page();
-	
-	$initial_restricion = html_entity_decode($colectionData->restriction);
-	// filtro de pesquisa = restricao inicial  E filtro where  E filtro externo E filtro(s) selecionados
-	$filterSearch = array_merge((array)$initial_restricion,(array)$whereFilter, (array)$filter,(array)$filter_chain);
+        if ( !isset($col) ){
+            $col = $defaultCollection; 				// valor default criada no script de configuracao do sistema
+            $VARS["col"] = $col;					// registra no array vars para uso na XSL
+        }
 
-	// set additiona parameters
-	$dia->setParam('fb',$fb);
-	$dia->setParam('fl',$fl);
-	$dia->setParam('sort',$sort);
+        if ( !isset($site) ){
+            $site= $defaultSite;					// valor default criada no script de configuracao do sistema
+            $VARS["site"] = $site;					// registra no array vars para uso na XSL
+        }
 
-	$diaResponse = $dia->search($q, $index, $filterSearch, $from);
-	$result = json_decode($diaResponse );
+        $index= $_REQUEST["index"];
+        $sort = $_REQUEST["sort"]; 		      		//sort parameter
+        $output = ( isset($_REQUEST["output"]) && $_REQUEST["output"] != '' ? $_REQUEST["output"] : "json" );
 
-	include("./templates/email-head.php");
-	include("./templates/email-top.php");
-	$page_body = $page->email();
-	include("./templates/email-bottom.php");
+        $VARS["count"] = $count;
 
-	$html_code = $page_head . $page_top . $page_body . $page_bottom; 
+        //DIA server connection object
+        $dia = new Dia($site, $col, $count, $output, $lang);
+        $page= new Page();
+
+        $initial_restricion = html_entity_decode($colectionData->restriction);
+        // filtro de pesquisa = restricao inicial  E filtro where  E filtro externo E filtro(s) selecionados
+        $filterSearch = array_merge((array)$initial_restricion,(array)$whereFilter, (array)$filter,(array)$filter_chain);
+
+        // set additiona parameters
+        $dia->setParam('fb',$fb);
+        $dia->setParam('fl',$fl);
+        $dia->setParam('sort',$sort);
+
+        $diaResponse = $dia->search($q, $index, $filterSearch, $from);
+        $result = json_decode($diaResponse );
+
+        include("./templates/email-head.php");
+        include("./templates/email-top.php");
+        $page_body = $page->email();
+        include("./templates/email-bottom.php");
+
+        $html_code = $page_head . $page_top . $page_body . $page_bottom;
+    }else{
+        include("./templates/email-head.php");
+        include("./templates/email-top.php");
+        include("./templates/email-bottom.php");
+        $html_code = $page_head . $page_top . $page_bottom;
+    }
 
 	// Envio do e-mail
 	$mail = new PHPMailer();
