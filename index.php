f430747d2e646eac4db2f24ccefd6ada92b982a1
viniciusandrade <viniciusdeandrade@gmail.com>
2010-12-13 17:27:27 +0000

    remove limit for exportation in RIS format (create a loop in result object)
    git-svn-id: http://svn.reddes.bvsalud.org/iahx/trunk/opac@354 80a35789-0352-4f55-950b-1782766af574

--- index.php
+++ index.php
@@ -1,52 +1,52 @@
 <?PHP
-	require_once("config.php");	
-	require_once("./classes/Dia.php");
-	require_once("./classes/Page.php");
-	require_once("./classes/Log.php");
-	require_once('./classes/smarty/Smarty.class.php');
+    require_once("config.php"); 
+    require_once("./classes/Dia.php");
+    require_once("./classes/Page.php");
+    require_once("./classes/Log.php");
+    require_once('./classes/smarty/Smarty.class.php');
 
-	$col = $_REQUEST["col"];
-	$site = $_REQUEST["site"];
-	$printMode = $_REQUEST["printMode"];
+    $col = $_REQUEST["col"];
+    $site = $_REQUEST["site"];
+    $printMode = $_REQUEST["printMode"];
 
-	if ( !isset($col) ){
-		$col = $defaultCollection; 				// valor default criada no script de configuracao do sistema
-	}
-	if ( !isset($site) ){	
-		$site= $defaultSite;					// valor default criada no script de configuracao do sistema
-	}
+    if ( !isset($col) ){
+        $col = $defaultCollection;              // valor default criada no script de configuracao do sistema
+    }
+    if ( !isset($site) ){   
+        $site= $defaultSite;                    // valor default criada no script de configuracao do sistema
+    }
 
-	$from= ( isset($_REQUEST["from"]) && $_REQUEST["from"] != ''? $_REQUEST["from"] : 0 );
-	
-	$q  = stripslashes($_REQUEST["q"]);			//query
-	
-	if ($q == $texts['ENTER_WORDS']){
-		$q = "";
-	}	
-	
-	$qt = $_REQUEST["qt"];						//query type
-	$index= $_REQUEST["index"];
+    $from= ( isset($_REQUEST["from"]) && $_REQUEST["from"] != ''? $_REQUEST["from"] : 0 );
+    
+    $q  = stripslashes($_REQUEST["q"]);         //query
+    
+    if ($q == $texts['ENTER_WORDS']){
+        $q = "";
+    }   
+    
+    $qt = $_REQUEST["qt"];                      //query type
+    $index= $_REQUEST["index"];
 
-	if (isset($_REQUEST['sort']) && $_REQUEST['sort'] != ""){
-		$sort = getSortValue($colectionData,$_REQUEST["sort"]);		//get sort field to apply
-	}else{
-		$sort = getDefaultSort($colectionData, $q);		//get default sort
-	}
-	$output = ( isset($_REQUEST["output"]) && $_REQUEST["output"] != '' ? $_REQUEST["output"] : "json" );
+    if (isset($_REQUEST['sort']) && $_REQUEST['sort'] != ""){
+        $sort = getSortValue($colectionData,$_REQUEST["sort"]);     //get sort field to apply
+    }else{
+        $sort = getDefaultSort($colectionData, $q);     //get default sort
+    }
+    $output = ( isset($_REQUEST["output"]) && $_REQUEST["output"] != '' ? $_REQUEST["output"] : "json" );
     $callback = $_REQUEST['callback'];          // append callback function to json output
 
     $media =  $_REQUEST["media"];               // media template: screen (default) or mobile
-		
-	$filter = $_REQUEST["filter"];         		//initial filter to apply
-	$filterLabel = $_REQUEST["filterLabel"];	//initial filter label
-	
-	$filter_chain = $_REQUEST["filter_chain"];	//user filter sequence (history)
-	$addFilter = $_REQUEST["addfilter"];   		//new filter to apply
-	$backFilter= $_REQUEST["backfilter"];  		//back to filter position
+        
+    $filter = $_REQUEST["filter"];              //initial filter to apply
+    $filterLabel = $_REQUEST["filterLabel"];    //initial filter label
+    
+    $filter_chain = $_REQUEST["filter_chain"];  //user filter sequence (history)
+    $addFilter = $_REQUEST["addfilter"];        //new filter to apply
+    $backFilter= $_REQUEST["backfilter"];       //back to filter position
 
-	$where = $_REQUEST["where"];							//select where search
-	$whereFilter = getWhereFilter($colectionData,$where);	//select where search
-	$count = ( isset($_REQUEST["count"]) ? $_REQUEST["count"] : $config->documents_per_page );
+    $where = $_REQUEST["where"];                            //select where search
+    $whereFilter = getWhereFilter($colectionData,$where);   //select where search
+    $count = ( isset($_REQUEST["count"]) ? $_REQUEST["count"] : $config->documents_per_page );
     
     $fmt = $_REQUEST["fmt"];                                //display format
 
@@ -60,60 +60,79 @@
     $csa['css'] = $_REQUEST["css"];
     $csa['display_banner'] = $_REQUEST["display_banner"];
 
-	//cluster parameters
-	$fl = $colectionData->cluster_items_limit;
-	$fb = $_REQUEST["fb"];									// facet browse
+    //cluster parameters
+    $fl = $colectionData->cluster_items_limit;
+    $fb = $_REQUEST["fb"];                                  // facet browse
 
-	if ( $addFilter != "" ){
-		$filter_chain[] = $addFilter;
-	}
-	if (isset($backFilter) && $backFilter != ""){
-		if ($backFilter == -1)	{  		//delete filter history
-			$filter_chain = null;
-		}else{
-			if ($backFilter == -2)	{  	//delete filter history and query
-				$filter_chain = null;
-				$q = null;
-			}else{
-				$reduceFilter = array_slice($filter_chain,0,$backFilter+1);		//delete filter history to position
-				$filter_chain = $reduceFilter;
-			}
-		}	
-	}
+    if ( $addFilter != "" ){
+        $filter_chain[] = $addFilter;
+    }
+    if (isset($backFilter) && $backFilter != ""){
+        if ($backFilter == -1)  {       //delete filter history
+            $filter_chain = null;
+        }else{
+            if ($backFilter == -2)  {   //delete filter history and query
+                $filter_chain = null;
+                $q = null;
+            }else{
+                $reduceFilter = array_slice($filter_chain,0,$backFilter+1);     //delete filter history to position
+                $filter_chain = $reduceFilter;
+            }
+        }   
+    }
 
-	$debug = $_REQUEST['debug'];
-	$detail = $_REQUEST['detail'];
+    $debug = $_REQUEST['debug'];
+    $detail = $_REQUEST['detail'];
 
-	//DIA server connection object
-	$dia = new Dia($site, $col, $count, $output, $lang);	
-	$page= new Page();
+    //DIA server connection object
+    $dia = new Dia($site, $col, $count, $output, $lang);    
+    $page= new Page();
 
-	$initial_restricion = html_entity_decode($colectionData->restriction);
-	// filtro de pesquisa = restricao inicial  E filtro where  E filtro externo E filtro(s) selecionados
-	$filterSearch = array_merge((array)$initial_restricion,(array)$whereFilter, (array)$filter,(array)$filter_chain);
+    $initial_restricion = html_entity_decode($colectionData->restriction);
+    // filtro de pesquisa = restricao inicial  E filtro where  E filtro externo E filtro(s) selecionados
+    $filterSearch = array_merge((array)$initial_restricion,(array)$whereFilter, (array)$filter,(array)$filter_chain);
 
-	// set additiona parameters
-	$dia->setParam('fb',$fb);
-	$dia->setParam('fl',$fl);
-	$dia->setParam('qt',$qt);
-	$dia->setParam('sort',$sort);
+    // set additiona parameters
+    $dia->setParam('fb',$fb);
+    $dia->setParam('fl',$fl);
+    $dia->setParam('qt',$qt);
+    $dia->setParam('sort',$sort);
 
-	$diaResponse = $dia->search($q, $index, $filterSearch, $from);
-	$result = json_decode($diaResponse);
+    if($output == "ris"){
 
-	if ($output == "xml" || $output == "sol"){
-		header("Content-type: text/xml; charset=UTF-8");
-		print $diaResponse;
-	}else if($output == "rss"){
-		header("Content-type: text/xml; charset=UTF-8");
-		$page->RSS();
-	}else if($output == "ris"){
-		header("Content-type: application/x-Research-Info-Systems; charset=UTF-8");
+        // create a loop for export all citation
+        header("Content-type: application/x-Research-Info-Systems; charset=UTF-8");
         header('Content-Disposition: attachment; filename="citation.ris"');
-		$page->RIS();
-	}else if($output == "metasearch"){
-		header("Content-type: text/xml; charset=UTF-8");
-		$page->MetaSearch();
+        $export_count = '100';
+        
+        $dia->setParam('count',$export_count);        
+
+        $diaResponse = $dia->search($q, $index, $filterSearch, 0);
+        $result = json_decode($diaResponse);
+        $num_found = intval($result->diaServerResponse[0]->response->numFound);
+        $page->RIS();
+        
+        for ($export_from = $export_count+1; $export_from <= $num_found; $export_from += $export_count){
+            $diaResponse = $dia->search($q, $index, $filterSearch, $export_from);
+            $result = json_decode($diaResponse);
+            $page->RIS();
+            print("num found: " . $num_found . " export_from: " . $export_from);
+        }
+        die();
+    }else{
+        $diaResponse = $dia->search($q, $index, $filterSearch, $from);
+        $result = json_decode($diaResponse);
+    }
+
+    if ($output == "xml" || $output == "sol"){
+        header("Content-type: text/xml; charset=UTF-8");
+        print $diaResponse;
+    }else if($output == "rss"){
+        header("Content-type: text/xml; charset=UTF-8");
+        $page->RSS();
+    }else if($output == "metasearch"){
+        header("Content-type: text/xml; charset=UTF-8");
+        $page->MetaSearch();
     }else if($output == "js"){
         header("Content-type: text/plain; charset=UTF-8");
         if (isset($callback) && ereg("^[a-z_]{1,25}$", $callback)){
@@ -121,25 +140,25 @@
         }else{
             echo $diaResponse;
         }
-	}else{		
-		// html output
-		$page->show();
-	}
+    }else{      
+        // html output
+        $page->show();
+    }
 
-	flush();
-	// log de pesquisas realizadas
-	$log = new Log();
-	$log->fields['ip']   = $_SERVER["REMOTE_ADDR"];
-	$log->fields['lang'] = $lang;
-	$log->fields['col']  = $col;
-	$log->fields['site'] = $site;
-	$log->fields['query']= ($q != ''? $q : "*");
-	$log->fields['index']= ($index != ''? $index : "*");
-	$log->fields['where']= ($_REQUEST['where'] != ''? $_REQUEST['where'] : "*");
-	$log->fields['filter'] = $dia->getFilterParam();
-	$page = (($from-1)/$count) + 1;
-	$log->fields['from'] = (strval($page) < 1? "1": $page);
+    flush();
+    // log de pesquisas realizadas
+    $log = new Log();
+    $log->fields['ip']   = $_SERVER["REMOTE_ADDR"];
+    $log->fields['lang'] = $lang;
+    $log->fields['col']  = $col;
+    $log->fields['site'] = $site;
+    $log->fields['query']= ($q != ''? $q : "*");
+    $log->fields['index']= ($index != ''? $index : "*");
+    $log->fields['where']= ($_REQUEST['where'] != ''? $_REQUEST['where'] : "*");
+    $log->fields['filter'] = $dia->getFilterParam();
+    $page = (($from-1)/$count) + 1;
+    $log->fields['from'] = (strval($page) < 1? "1": $page);
 
-	$log->writeLog();
+    $log->writeLog();
 
 ?>
