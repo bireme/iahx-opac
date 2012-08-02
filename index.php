2ac460a441e5af2ac39d6d984629acee9de2b550
moacirmoda <moa.moda@gmail.com>
2012-08-02 18:46:34 -0300

    moving languages to locale, cleaning files and some adjusts in environment

--- index.php
+++ index.php
@@ -2,25 +2,12 @@
 
 require_once 'environment.php';
 
-use Silex\Provider\SessionServiceProvider;
-
-// default translation
-$texts = parse_ini_file(__DIR__ . "/languages/" . $lang . "/texts.ini", true);
-
-// registering sessions
-$app->register(new SessionServiceProvider, array(
-    'session.storage.save_path' => '/tmp/sessions/iahx',
-    'session.storage.options' => array(
-        'name' => 'iahx',
-    ),
-));
-
-$app['twig']->addFunction('custom_template', new Twig_Function_Function('custom_template'));
-
-// incluindo as views
+// include all views created in views path
 foreach(glob(VIEWS_PATH . "*.php") as $file) {
     require_once $file;
 }
 
+// running the framework
 $app->run();
+
 ?>
