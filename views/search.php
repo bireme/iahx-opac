69f25986c98c4e4d16b5311d55b13f53ec5400f6
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2012-12-04 16:07:25 -0200

    Fix search url on email export

--- views/search.php
+++ views/search.php
@@ -262,10 +262,9 @@ $app->match('/', function (Request $request) use ($app, $DEFAULT_PARAMS, $config
     // if is send email
     if(isset($params['is_email'])) {
 
-        $output_array['request_uri'] = $_SERVER['REQUEST_URI'];
         $output_array['email'] = $email;
 
-        $render = $app['twig']->render('export-email.html', $output_array);
+        $render = $app['twig']->render('export-email.html', $output_array);        
         $subject = ($email['subject'] != '' ? $email['subject'] : $texts['SEARCH_HOME'] . ' | ' . $texts['BVS_TITLE']);
 
         $message = \Swift_Message::newInstance()
