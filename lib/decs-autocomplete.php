f263c1c64ac79a977d614fd2334e12f2daf0e17b
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2013-10-08 15:45:21 -0300

    Remove term name duplication when present descriptors at locator page. Use service quickterm count param

--- lib/decs-autocomplete.php
+++ lib/decs-autocomplete.php
@@ -3,7 +3,7 @@
 include('functions.php');
 
 $query = trim($_REQUEST['query']);
-$count = ( isset($_REQUEST['count']) ? intval($_REQUEST['count']) : 20 );
+$count = $_REQUEST['count'];
 $query = str_replace(" ","+",$query);
 $query = remove_accents($query);
 $query = $query . "*";
@@ -12,21 +12,24 @@ $jsoncallback = $_REQUEST['callback'];
 
 $lang = trim($_REQUEST['lang']);
 
-$service_url = "http://srv.bvsalud.org/decsQuickTerm/search?query=" . $query . "&lang=" . $lang;
+$service_url = "http://srv.bvsalud.org/decsQuickTerm/search?query=" . $query . "&count=" . $count . "&lang=" . $lang;
 	
 $service_response = file_get_contents($service_url);
 $xml = simplexml_load_string($service_response);
 
 
 $descriptors = array();
-$i = 0;
+$term_list = array();
 
 foreach ($xml->Result->item as $item ) {
     $attr = $item->attributes();
-    $descriptors[] = array('name' => (string)$attr['term'], 'id' => (string)$attr['id']);     
-    $i++;
-    if ($i >= $count){
-        break;
+    $term_name = (string)$attr['term'];
+    $term_id = (string)$attr['id'];
+
+    // remove duplications of term name (when term has same name in other languages)
+    if ( !in_array($term_name, $term_list)) {
+        $descriptors[] = array('name' => $term_name, 'id' => $term_id);  
+        $term_list[] = $term_name;   
     }
 }
 
