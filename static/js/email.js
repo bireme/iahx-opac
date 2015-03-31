b8ef12ce0eae2a1f05d944b9d8f894a1dd62d4b4
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2015-03-31 13:49:33 -0300

    Fix bug at email form. Close #6

--- static/js/email.js
+++ static/js/email.js
@@ -1,13 +1,21 @@
 function send_email() {
     var form = document.emailForm;
 
-    if( form["email[]"].value === "") {
-        alert("Todos os campos com asterisco (*) devem ser preenchidos.");
+    var dest_emails = '';
+    $("input[name='email[]']").each(function() {
+        var value = $(this).val();
+        if (value != '') {
+            dest_emails += value;
+        }
+    });
+
+    if( dest_emails === "") {
+        alert("Informe o email para envio.");
         return false;
     }
 
-    if( form["email[]"].value.search("@") < 0) {
-        alert("Por favor, preencha com emails válidos.");
+    if( dest_emails.search("@") < 0) {
+        alert("Informe um email válido.");
         return false;   
     }
     
@@ -15,9 +23,10 @@ function send_email() {
 }
 
 function add_more_email() {
-    var dest = $("#destinatarios");
-    var input = dest.html();
+
+    var dest = $("#destinatarios").clone();
+    var new_input = dest.html();
     var more_dest = $("#more-destinatarios");
 
-    more_dest.html(more_dest.html() + input);
+    more_dest.html(more_dest.html() + new_input);
 }
