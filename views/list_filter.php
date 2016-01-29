eb70b4212e2af25f6009452f5a16486cb804f54e
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2016-01-29 18:27:46 -0200

    Minor adjusts

---
+++ views/list_filter.php
@@ -0,0 +1,51 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+$app->get('list-filter/{filter_id}', function (Request $request, $filter_id) use ($app, $DEFAULT_PARAMS, $config) {
+    global $lang, $texts;
+
+    $collectionData = $DEFAULT_PARAMS['defaultCollectionData'];
+    $site = $DEFAULT_PARAMS['defaultSite'];
+    $col = $DEFAULT_PARAMS['defaultCollection'];
+    $filter = array();
+
+    $q = $request->get("q");
+    $index = $request->get("index");
+    $filter_param = $request->get("filter");
+    if ( !empty($filter_param) ){
+        $filter = $filter_param;
+    }
+    $count = $request->get("count");
+    // if not set count list all itens from filter
+    if ($count == ''){
+        $count = 999999;
+    }
+    // filter browse param (ex. au:10)
+    $fb = $filter_id . ":" . $count;
+
+    $dia = new Dia($site, $col, 1, 'site', $lang);
+    $dia->setParam('fb', $fb);
+
+    $dia_response = $dia->search($q, $index, $filter);
+    $result = json_decode($dia_response, true);
+
+    $filter_list = $result['diaServerResponse'][0]['facet_counts']['facet_fields'][$filter_id];
+
+    // translation file
+    $texts = parse_ini_file(TRANSLATE_PATH . $lang . "/texts.ini", true);
+
+    // output vars
+    $output_array = array();
+    $output_array['lang'] = $lang;
+    $output_array['config'] = $config;
+    $output_array['texts'] = $texts;
+    $output_array['filter_id'] = $filter_id;
+    $output_array['filter_list'] = $filter_list;
+
+
+    return $app['twig']->render(custom_template('list-filter.html'), $output_array);
+
+});
+
+?>
