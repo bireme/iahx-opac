faaef5c8ee97b6459c38aaf03293921f9cc8b2f6
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2020-03-26 17:19:13 -0300

    Atualização barra de acessibilidade. Close #155

--- static/regional/js/accessibility.js
+++ static/regional/js/accessibility.js
@@ -47,15 +47,15 @@ document.onkeydown=function(e){
 		pressedALT = true;
 	}
 	// Main Alt + 1
-	if(e.which == 49 && pressedALT == true) {
+	if((e.which == 49 || e.which == 97) && pressedALT == true) {
 		window.location.assign("#main_container");
 	}
 	//Pesquisa ALT + 2
-	if(e.which == 50 && pressedALT == true) {
+	if((e.which == 50 || e.which == 98) && pressedALT == true) {
 		$("#q").focus();
 	}
 	//Footer ALT + 3
-	if(e.which == 51 && pressedALT == true) {
+	if((e.which == 51 || e.which == 100) && pressedALT == true) {
 		window.location.assign("#footer");
 	}
 }
