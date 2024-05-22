87de6d91235b7cbae9773d064602fae3f96c43bc
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2024-05-22 15:05:17 -0300

    Ajustes para uso de mecanismo de cache (REDIS)

---
+++ src/Service/CacheService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class CacheService
+{
+    public function __construct(
+        private string $projectDir,
+        private CacheInterface $cache,
+    ){}
+
+    function get_texts($instance, $lang) {
+        // cache key id
+        $texts_cache_key = "texts_" . $lang . "_" . $instance;
+
+        // Fetch the texts from cache or generate them if not cached
+        $texts = $this->cache->get($texts_cache_key, function (ItemInterface $item) use ($instance, $lang): array {
+            $translation_path = $this->projectDir . '/instances/' . $instance . '/translations/' . $lang;
+            $texts_ini = parse_ini_file($translation_path . "/texts.ini", true);
+            return $texts_ini;
+        });
+
+        return $texts;
+    }
+
+    function get_texts_decs_locator($lang) {
+        // Create the cache key
+        $texts_cache_key = "texts_" . $lang . "_decslocator";
+
+        // Fetch the texts from cache or generate them if not cached
+        $texts = $this->cache->get($texts_cache_key, function (ItemInterface $item) use ($lang): array {
+            $translation_path = $this->projectDir . '/translations/' . $lang;
+            $texts_ini = parse_ini_file($translation_path . "/decs-locator.ini", true);
+            return $texts_ini;
+        });
+
+        return $texts;
+    }
+
+    function get_config($instance) {
+        // cache key id
+        $config_cache_key = "config_" . $instance;
+
+        // Fetch the texts from cache or generate them if not cached
+        $config_str = $this->cache->get($config_cache_key, function (ItemInterface $item) use ($instance) {
+            $config_file = $this->projectDir . '/instances/' . $instance . '/config/config.xml';
+
+            $config_xml = simplexml_load_file($config_file, 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
+            // Convert SimpleXMLElement to string for serialization
+            $config_xml_str = $config_xml->asXML();
+            return $config_xml_str;
+        });
+
+        // Convert string to SimpleXMLElement
+        $config_xml = simplexml_load_string($config_str);
+
+        return $config_xml;
+    }
+
+
+}
+?>
