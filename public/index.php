fe663eb8774d641a2abb62c996e31d444bf18d17
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2024-06-17 13:38:53 -0300

    Permite custom.css e logo customizado por instância

--- public/index.php
+++ public/index.php
@@ -5,6 +5,16 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
+return function (array $context): Kernel {
+    $path_info = explode("/", $_SERVER['REQUEST_URI']);
+
+    // update INSTANCE environment using path_info
+    if (isset($path_info[1]) ) {
+        $instance = $path_info[1];
+        $context['INSTANCE'] = $instance;
+        $_ENV['INSTANCE'] = $instance;
+        putenv("INSTANCE=" . $instance);
+    }
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
